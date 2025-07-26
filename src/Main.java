757eb40dd418cc426e553595415669e14db744c2
webManiac <wankhade.ashish@yahoo.com>
2025-07-26 11:49:28 +0530

    Feature | design pattern [singleton]

---
+++ src/Main.java
@@ -0,0 +1,10 @@
+import com.designpatterns.creational.Singleton;
+
+public class Main {
+    public static void main(String[] args) {
+        Singleton obj1 = Singleton.getInstance();
+        Singleton obj2 = Singleton.getInstance();
+
+        System.out.println(obj1 == obj2);
+    }
+}
